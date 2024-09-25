108c466b6cdcc0a6aecdb38da0338814b5f65106
evanmoua <826666@mystma.org>
2024-09-25 09:51:39 -0500

    Converting

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,8 +1,9 @@
-french_fries = input (" What is your favorite animal?\n")   # My Question 1  
-hamster = input ("Why do you like them?\n")                 # My Question 2 
-user_input3 = input ( "where do they live?\n")              # My Question 3 
-user_input4 = input ("What is a fun fact about Them?\n")     # My Question 4 
-user_input5 = input ("What color are they?\n")              # My Question 5 
+french_fries = input (" What is your favorite animal?\n")   # My Question 1  Talking about your Favorite Animals
+hamster = input ("Why do you like them?\n")                 # My Question 2  About Why do you like that animals 
+user_input3 = input ( "where do they live?\n")              # My Question 3   where they live 
+user_input4 = input ("What is a fun fact about Them?\n")     # My Question 4  Likes facts about them
+user_input5 = input ("What color are they?\n")              # My Question 5  There color fur/ coat 
 print("your favorite animal is the " + french_fries +  "because"  + hamster + "." +   "they live in " + user_input3 + "and" + user_input4 + "they are" + user_input5)
 # My Question 1-5 / The Whole question/ sentences above ^
- 
+   
+   
