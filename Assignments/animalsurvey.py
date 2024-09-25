c0ca3f66a174bef653cd1fb9a8c3b991a86c03d5
evanmoua <826666@mystma.org>
2024-09-25 09:30:31 -0500

    MY Comments

--- Assignments/animalsurvey.py
+++ Assignments/animalsurvey.py
@@ -1,6 +1,8 @@
-french_fries = input (" What is your favorite animal?\n")
-hamster = input ("Why do you like them?\n")
-user_input3 = input ( "where do they live?\n")
-user_input4 = input ("What is a fun fact about Them?\n")
-user_input5 = input ("What color are they?\n")
-print("your favorite animal is the " + french_fries + "because" + hamster + "." + "they live in " + user_input3 + "and" + user_input4 + "they are" + user_input5)
+french_fries = input (" What is your favorite animal?\n")   # My Question 1  
+hamster = input ("Why do you like them?\n")                 # My Question 2 
+user_input3 = input ( "where do they live?\n")              # My Question 3 
+user_input4 = input ("What is a fun fact about Them?\n")     # My Question 4 
+user_input5 = input ("What color are they?\n")              # My Question 5 
+print("your favorite animal is the " + french_fries +  "because"  + hamster + "." +   "they live in " + user_input3 + "and" + user_input4 + "they are" + user_input5)
+# My Question 1-5 / The Whole question/ sentences above ^
+ 
