efe3dbf8f9e3d506a3f805393af19dbea3946c84
evanmoua <826666@mystma.org>
2024-09-25 09:00:50 -0500

    Animal survey

---
+++ Practice/My practice_sept-24.py
@@ -0,0 +1,13 @@
+num = input("Entenum = input("Enter the number you want to sqaure:")
+num = int(num)
+print(num * num)r something!\n>") # Asking for input and declaring a varable 
+num  = int(Num)                      
+print(num * 10)
+
+
+
+
+
+print("Welcome to word Smash!")
+word_one = input("Enter the one word!\n>")
+Word_two = input ("Enter the second word!\n>")
