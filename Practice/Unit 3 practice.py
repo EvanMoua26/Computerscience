6f5daf94ce1c20b87eeee3cbe3de659343a63f78
evanmoua <826666@mystma.org>
2024-10-04 09:41:47 -0500

    Boolean HW

---
+++ Practice/Unit 3 practice.py
@@ -0,0 +1,30 @@
+ 
+print(5 > 10)
+# values: 5, 10
+# realtional operator: >
+
+x = "blue"
+print(x == "yellow")
+# values: x, "yellow"
+# realtional operator: ==
+
+answer = "cube"
+count = 1
+print(answer == "cube" and count < 10)
+
+# values: answer, "cube", count, 10
+# realtional operators: ==, <
+# logical operators: and
+
+
+
+
+
+
+
+
+# **Equal to (==)**: Checks if two operands are equal. For example, `5 == 3` results in `false`.
+  # **Greater than (>)**: Checks if the first operand is greater than the second. For example, `5 > 3` results in `true`.
+ # **Less than (<)**: Checks if the first operand is less than the second. For example, `5 < 3` results in `false`.
+#**Greater than or equal to (>=)**: Checks if the first operand is greater than or equal to the second. For example, `5 >= 3` results in `true`.
+ # **Less than or equal to (<=)**: Checks if the first operand is less than or equal to the second. For example, `5 <= 3` results in `false`. 
